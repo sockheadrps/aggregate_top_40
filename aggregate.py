509bbd3408eeee4ef6b80b2a6afedc4c60d852d9
sockheadrps <39883642+sockheadrps@users.noreply.github.com>
2022-10-21 10:15:59 -0400

    Update aggregate.py

--- aggregate.py
+++ aggregate.py
@@ -1,21 +1,37 @@
 import requests
 import bs4
 
-url = r"https://www.musixmatch.com/explore/genre/Hip-Hop-Rap"
+url = r"https://www.musixmatch.com/explore/genre/Country"
+urls = [r"https://www.musixmatch.com/explore/genre/Country",
+		r"https://www.musixmatch.com/explore/genre/Alternative",
+		r"https://www.musixmatch.com/explore/genre/Dance",
+		r"https://www.musixmatch.com/explore/genre/Pop",
+		r"https://www.musixmatch.com/explore/genre/Rock"
 
-r = requests.get(url, headers={'User-Agent': 'Custom'})
 
-soup = bs4.BeautifulSoup(r.content, 'html.parser')
-a_tags = (soup.findAll("a", {'class': "title"}, href=True))
 
-links = []
-for a in a_tags:
-	links.append(f"https://www.musixmatch.com{a['href']}")
+]
 
-with open('lyrics.txt', 'w') as f:
-	for link in links:
-		r = requests.get(link, headers={'User-Agent': 'Custom'})
-		soup = bs4.BeautifulSoup(r.content, 'html.parser')
-		lyrics = (soup.findAll("span", {'class': "lyrics__content__ok"}))
-		for item in lyrics:
-			f.write(item.text.encode("ascii", 'ignore').decode('utf-8'))
+for u in urls:
+	r = requests.get(u, headers={'User-Agent': 'Custom'})
+
+	soup = bs4.BeautifulSoup(r.content, 'html.parser')
+	a_tags = (soup.findAll("a", {'class': "title"}, href=True))
+
+	print(a_tags)
+	links = []
+	for a in a_tags:
+		links.append(f"https://www.musixmatch.com{a['href']}")
+		print(a)
+
+	with open('lyrics.txt', 'a') as f:
+		for link in links:
+			r = requests.get(link, headers={'User-Agent': 'Custom'})
+			soup = bs4.BeautifulSoup(r.content, 'html.parser')
+			lyrics = (soup.findAll("span", {'class': "lyrics__content__ok"}))
+			for item in lyrics:
+				print(item)
+				f.write(item.text.encode("ascii", 'ignore').decode('utf-8'))
+
+
+
